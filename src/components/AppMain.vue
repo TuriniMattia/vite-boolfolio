f1d47db0712387b1a6c8e383a6ec845cb0690f46
TuriniMattia <turinimattia96@gmail.com>
2024-06-17 16:16:22 +0200

    [add]scaffolding base + chiamata axios

---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Progetti</h2>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+
+
+  data() {
+
+
+    return {
+      projects: [],
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+
+
+}
+</script>
+
+<style lang="scss" scoped></style>
